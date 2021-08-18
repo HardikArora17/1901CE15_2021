204268b6feb97e897d267f837c096f20ae76d8c5
HardikArora17 <54908862+HardikArora17@users.noreply.github.com>
2021-08-18 20:35:23 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,32 @@
-def meraki_helper(n):
-    """This will detect meraki numner"""
 
+def meraki_helper(input_l):
+    
+    input_list=input_l
+    sumy=0
+    sumn=0
+
+    for number in input_list:
+        n=str(number)
+        flag="Yes"
+        
+        for i in range(1,len(n)):
+            if(abs(int(n[i-1])-int(n[i]))==1):
+                continue
+            else:
+                flag="No"
+                break
+            
+        if(flag=="Yes"):
+            sumy+=1
+            print(flag," - ",n,"is a Meraki Number")
+            
+        else:
+            sumn+=1
+            print(flag," - ",n,"is not a Meraki Number")
+            
+            
+    print("the input list contains",sumy,"meraki numbers and",sumn,"non meraki numbers")
+    
 
 input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+meraki_helper(input)
